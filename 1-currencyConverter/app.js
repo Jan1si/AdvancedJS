76180a08e7489273ba79ca3fe9e9f4411d0ce36a
Nikita <nikita.evgrafov25@gmail.com>
2025-07-05 20:08:53 +0500

    finish practic work

--- 1-currencyConverter/app.js
+++ 1-currencyConverter/app.js
@@ -1,1 +1,34 @@
-console.log("Hello converter");
+'use strict'
+
+const converter = (sum, initCurrency, convertCurrency) => {
+    const allCurrency = [
+        {name: "RUB", price: 1, synonym: ["rub", "рубль", "рубли", "рублей"], locale: "ru-RU"},
+        {name: "USD", price: 80, synonym: ["usd", "доллар", "доллары", "долларов"], locale: "en-US"},
+        {name: "EUR", price: 90, synonym: ["eur", "евро"], locale: "de-DE"}
+    ];
+
+    const validInit  =  allCurrency.find(currency => {
+        if (initCurrency !== currency.name) {
+            return currency.synonym.find(syn => syn === initCurrency);
+        } 
+        return currency.name;
+    });
+
+    const validConvert  =  allCurrency.find(currency => {
+        if (convertCurrency !== currency.name) {
+            return currency.synonym.find(syn => syn === convertCurrency);
+        } 
+        return currency.name;
+    });
+
+    if(!validInit || !validConvert){
+        return null;
+    }
+
+    return new Intl
+                .NumberFormat(validInit.locale, {style: "currency", currency: validConvert.name})
+                .format(sum * validInit.price / validConvert.price);
+    
+}
+
+console.log(converter(100, "евро", "рубли"));
