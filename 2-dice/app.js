c9d5260e65dac15a623ee82b2c195b22b7642b13
Nikita <nikita.evgrafov25@gmail.com>
2025-07-05 21:23:56 +0500

    исправил критические ошибки

--- 2-dice/app.js
+++ 2-dice/app.js
@@ -1,5 +1,5 @@
-const randomNum = (min = 1, max) => {
-    return Math.floor(Math.random() * (max - min) + 1) + min;
+const randomNum = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 console.log(randomNum(1, 5));
