cbffe86d1826d58a3d23cfcba63b4c9ca9fa426b
Nikita <nikita.evgrafov25@gmail.com>
2025-07-05 21:19:04 +0500

    готовое домашнее задание 2-dice

---
+++ 2-dice/app.js
@@ -0,0 +1,18 @@
+const randomNum = (min = 1, max) => {
+    return Math.floor(Math.random() * (max - min) + 1) + min;
+}
+
+console.log(randomNum(1, 5));
+
+const dropDice = (dice) => {
+    const diceNum = Number.parseInt(dice.split('d').join(''));
+    return randomNum(1, diceNum)
+}
+
+console.log(dropDice("d4"));
+console.log(dropDice("d6"));
+console.log(dropDice("d8"));
+console.log(dropDice("d10"));
+console.log(dropDice("d12"));
+console.log(dropDice("d16"));
+console.log(dropDice("d20"));
