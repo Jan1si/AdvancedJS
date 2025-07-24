596977bc03591640dfc6b21a45aac2c5f425fae4
Nikita <nikita.evgrafov25@gmail.com>
2025-07-24 22:10:09 +0500

    Полностью выполнил задание

--- classes/app.js
+++ classes/app.js
@@ -1,13 +1,19 @@
 'use strict';
 
-class Person {
-    #createAt
 
-    constructor(name, age, createAt){
+class Person {
+    #createAt;
+    static #countPersone = 0;
+    constructor(name, age){
         this.name = name;
         this.age = age;
-        this.#createAt = createAt;
+        this.#createAt = new Date();
+        Person.#countPersone++;
     };
+
+    static get totalPerson(){
+        return Person.#countPersone;
+    }
 
     static isAdult(age) {
         return age >= 18;
@@ -43,7 +49,7 @@ class Person {
 }
 
 // Тестирование класса Person
-const person = new Person('Ivan', 20, new Date());
+const person = new Person('Ivan', 20);
 console.log(person.createAt);
 person.speak();
 console.log(Person.isAdult(18));
@@ -53,8 +59,8 @@ console.log(Person.isAdult(17));
 class Student extends Person{
     #spells = new Set();
 
-    constructor(name, age, createAt){
-        super(name, age, createAt);
+    constructor(name, age){
+        super(name, age);
     };
 
     get spells(){
@@ -64,16 +70,20 @@ class Student extends Person{
     learnSpell(nameSpell){
         if(!this.#spells.has(nameSpell)){
             this.#spells.add(nameSpell);
-            return `${this.name} выучил новое заклинание "${nameSpell}"`;
+            console.log(`${this.name} выучил новое заклинание "${nameSpell}"`);
+            return;
         }
-        return `${this.name} уже знает это заклинане!`;
+        console.log(`${this.name} уже знает это заклинане!`);
+        return;
     };
 
     castSpell(nameSpell){
         if (!this.#spells.has(nameSpell)){
-            return `${this.name} не знает такого заклинания!`
+            console.log(`${this.name} не знает такого заклинания!`);
+            return;
         }
-        return `${this.name} кастует заклинание ${nameSpell}!`
+        console.log(`${this.name} кастует заклинание ${nameSpell}!`);
+        return;
     };
 
     speak(){
@@ -83,33 +93,35 @@ class Student extends Person{
 }
 
 //Тестирование класса Student
-const student = new Student('Кирилл', 18, new Date());
-const student2 = new Student('Мария', 17, new Date());
-const student3 = new Student('Сергей', 19, new Date());
-console.log(student.learnSpell('Огненный шар'));
-console.log(student.learnSpell('Огненный шар'));
-console.log(student.learnSpell('Цепная молния'));
-console.log(student.learnSpell('Снежная буря'));
-console.log(student2.learnSpell('Цепная молния'));
-console.log(student2.learnSpell('Снежная буря'));
-console.log(student.castSpell('Огненный шар'));
-console.log(student.castSpell('Ледяной шар'));
+const student = new Student('Кирилл', 18);
+const student2 = new Student('Мария', 17);
+const student3 = new Student('Сергей', 19);
+student.learnSpell('Огненный шар');
+student.learnSpell('Огненный шар');
+student.learnSpell('Цепная молния');
+student.learnSpell('Снежная буря');
+student2.learnSpell('Цепная молния');
+student2.learnSpell('Снежная буря');
+student.castSpell('Огненный шар');
+student.castSpell('Ледяной шар');
 student.speak();
 //Тестирование класса Student
 
 class Teacher extends Person{
     #students = new Map();
 
-    constructor(name, age, createAt){
-        super(name, age, createAt);
+    constructor(name, age){
+        super(name, age);
     };
 
     addStudent(student){
         if (!this.#students.has(student.name)){
             this.#students.set(student.name, student);
-            return `${this.name} обучает нового студента ${student.name}`;
+            console.log(`${this.name} обучает нового студента ${student.name}`);
+            return;
         }
-        return `У ${this.name} уже есть ученик ${student.name}`;
+        console.log(`У ${this.name} уже есть ученик ${student.name}`);
+        return;
     };
 
     listStudents(){
@@ -128,40 +140,47 @@ class Teacher extends Person{
             console.log(`У учителя ${this.name} нет такого студента!`);
             return;
         };
-        console.log('Начало экзамена!');
         
         const foundStudent = this.#students.get(studentName);
         const spellsStudet = Array.from(foundStudent.spells);
+
+        if (spellsStudet.length === 0){
+            console.log(`Студент ${foundStudent.name} не знает заклинаний!`);
+            return;
+        }
+
         let attempt = 0;
+        console.log('Начало экзамена!');
 
         const interval = setInterval(() => {
             const randomIndexSpell = Math.floor(Math.random() * spellsStudet.length);
-            console.log(foundStudent.castSpell(spellsStudet[randomIndexSpell]));
+            foundStudent.castSpell(spellsStudet[randomIndexSpell]);
             attempt += 1;
             if (attempt === 5) {
                 console.log('Конец экзамена!');
-                
                 clearInterval(interval);
             } 
         }, 1000);
-
         
     }
 
     speak(){
         super.speak();
-        console.log('Я преподователь Академии!')
+        console.log('Я преподаватель Академии!')
     }
 }
 
 // Тестирование класса Teacher
-const teacher = new Teacher('Степан', 40, new Date());
-console.log(teacher.addStudent(student));
-console.log(teacher.addStudent(student2));
-console.log(teacher.addStudent(student3));
-console.log(teacher.addStudent(student));
+const teacher = new Teacher('Степан', 40);
+teacher.addStudent(student);
+teacher.addStudent(student2);
+teacher.addStudent(student3);
+teacher.addStudent(student);
 teacher.listStudents();
 teacher.speak();
 teacher.exam('Кирилл');
 
+console.log('____________________________');
+console.log(Person.totalPerson);
+console.log('____________________________');
 // Тестирование класса Teacher
