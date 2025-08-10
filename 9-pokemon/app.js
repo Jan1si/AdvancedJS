8c0fe2ad9f8451fbeb302509aa358e40fb1eabf0
Nikita <nikita.evgrafov25@gmail.com>
2025-08-10 18:17:01 +0500

    решил задание по запросам

---
+++ 9-pokemon/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+const request = new XMLHttpRequest();
+request.open('GET', url);
+request.send();
+request.addEventListener('load', function(){
+    const { abilities } = JSON.parse(this.responseText);
+    
+    const url = abilities[0].ability.url;
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.send();
+    request.addEventListener('load', function() {
+        const data = JSON.parse(this.responseText);
+        console.log(data.effect_entries[1].effect);  
+    })    
+});
+
+
