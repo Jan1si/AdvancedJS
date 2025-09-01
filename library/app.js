17fa27739385c5109d89f1acec404ea87efc4261
Nikita <nikita.evgrafov25@gmail.com>
2025-09-01 18:20:56 +0500

    Реализовал интерфейсы и базовые классы

--- library/app.js
+++ library/app.js
@@ -1,13 +1,108 @@
 'use strinct';
 // интерфейсы репозиториев User и Book 
-class InterfaceUserRepository{}
-class InterfaceBookRepository{}
+class InterfaceUserRepository{
+    constructor() {
+        if (new.target === InterfaceUserRepository){
+            throw new Error('Class cannon be initialized')
+        }
+    }
+    async registerUser(user){
+        throw new Error('Method not implemented')
+    }
+}
+
+class InterfaceBookRepository{
+    constructor(){
+        if (new.target === InterfaceBookRepository){
+            throw new Error('Class cannon be initialized');
+        }
+    }
+
+    async getBookByISBN(ISBN){
+        throw new Error('Method not implemented')
+    }
+
+    async getAllBooks(){
+        throw new Error('Method not implemented')
+    }
+
+    async addBook(book){
+        throw new Error('Method not implemented')
+    }
+}
 
 // классы для работы приложения 
-class User{}
-class Book{}
-class UserRepository extends InterfaceUserRepository{}
-class BookRepository extends InterfaceBookRepository{}
+class User{
+    #userId;
+    #name;
+    constructor(name) {
+        this.#userId = Math.random().toString(36).slice(2);
+        this.#name = name
+    }
+    get userId(){
+        return this.#userId;
+    }
+    get name(){
+        return this.#name;
+    }
+}
+
+class Book{
+    #isbn;
+    #name;
+    #author;
+    #genre;
+    constructor(name, author, genre){
+        this.#isbn = Math.random().toString(36).slice(2);
+        this.#name = name;
+        this.#author = author;
+        this.#genre = genre;
+    }
+    get isbn(){
+        return this.#isbn;
+    }
+}
+
+class UserRepository extends InterfaceUserRepository{
+    #users = new Map();
+
+    async registerUser(user){
+        if (!(user instanceof User)) {
+            throw new Error('Invalid class')
+        }
+               
+        if (Array.from(this.#users.keys()).includes(user.userId)){
+            throw new Error('User already exists');
+        }
+
+        this.#users.set(user.userId, user);
+        console.log(`User ${user.name} has be register`);
+    }
+
+    get users(){
+        return Array.from(this.#users);
+    }
+}
+
+class BookRepository extends InterfaceBookRepository{
+    #books = new Map();
+
+    async addBook(book) {
+        if (!(book instanceof Book)) {
+            throw new Error('Invalid class')
+        }
+        if (Array.from(this.#books.keys()).includes(book.isbn)) {
+            throw new Error('Book already exists')
+        }
+
+        this.#books.set(book.isbn, book);
+        console.log('Book has be added');   
+    }
+
+    get books() {
+        return Array.from(this.#books);
+    }
+}
 
 // класс бизнесс логики
 class LibraryServise{
